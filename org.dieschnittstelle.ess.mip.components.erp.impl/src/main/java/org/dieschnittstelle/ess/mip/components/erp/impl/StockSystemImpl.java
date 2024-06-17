816db7f4a19e61115b46c94ec0e8e8de2d85b6fd
Oemer Bilgit <s67173@bht-berlin.de>
2024-06-17 21:46:19 +0200

    finished mip + jpa 3 + 4

---
+++ org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/impl/StockSystemImpl.java
@@ -0,0 +1,70 @@
+package org.dieschnittstelle.ess.mip.components.erp.impl;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
+import org.dieschnittstelle.ess.entities.erp.PointOfSale;
+import org.dieschnittstelle.ess.entities.erp.StockItem;
+import org.dieschnittstelle.ess.mip.components.erp.api.StockSystem;
+import org.dieschnittstelle.ess.mip.components.erp.crud.api.PointOfSaleCRUD;
+import org.dieschnittstelle.ess.mip.components.erp.crud.impl.StockItemCRUD;
+import org.dieschnittstelle.ess.utils.interceptors.Logged;
+
+import java.util.List;
+
+@ApplicationScoped
+@Logged
+@Transactional
+public class StockSystemImpl implements StockSystem {
+
+    @Inject
+    private PointOfSaleCRUD posCRUD;
+
+    @Inject
+    private StockItemCRUD stockItemCRUD;
+
+    @Override
+    public void addToStock(IndividualisedProductItem product, long pointOfSaleId, int units) {
+        PointOfSale pos = posCRUD.readPointOfSale(pointOfSaleId);
+        StockItem stockItem = stockItemCRUD.readStockItem(product, pos);
+
+        if(stockItem != null) {
+            stockItem.setUnits(stockItem.getPrice() + units);
+            stockItemCRUD.updateStockItem(stockItem);
+        } else {
+            stockItem = new StockItem(product, pos, units);
+            stockItemCRUD.createStockItem(stockItem);
+        }
+    }
+
+    @Override
+    public void removeFromStock(IndividualisedProductItem product, long pointOfSaleId, int units) {
+
+    }
+
+    @Override
+    public List<IndividualisedProductItem> getProductsOnStock(long pointOfSaleId) {
+        return List.of();
+    }
+
+    @Override
+    public List<IndividualisedProductItem> getAllProductsOnStock() {
+        return List.of();
+    }
+
+    @Override
+    public int getUnitsOnStock(IndividualisedProductItem product, long pointOfSaleId) {
+        return 0;
+    }
+
+    @Override
+    public int getTotalUnitsOnStock(IndividualisedProductItem product) {
+        return 0;
+    }
+
+    @Override
+    public List<Long> getPointsOfSale(IndividualisedProductItem product) {
+        return List.of();
+    }
+}
--- org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/impl/StockSystemServiceImpl.java
+++ org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/impl/StockSystemServiceImpl.java
@@ -1,9 +1,12 @@
 package org.dieschnittstelle.ess.mip.components.erp.impl;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
+import org.dieschnittstelle.ess.mip.components.erp.api.StockSystem;
 import org.dieschnittstelle.ess.mip.components.erp.api.StockSystemService;
+import org.dieschnittstelle.ess.mip.components.erp.crud.api.ProductCRUD;
 import org.dieschnittstelle.ess.utils.interceptors.Logged;
 
 import java.util.List;
@@ -13,9 +16,16 @@ import java.util.List;
 @Logged
 public class StockSystemServiceImpl implements StockSystemService {
 
+    @Inject
+    private StockSystem stockSystem;
+
+    @Inject
+    private ProductCRUD productCRUD;
+
     @Override
     public void addToStock(long productId, long pointOfSaleId, int units) {
-
+        IndividualisedProductItem prod = (IndividualisedProductItem) productCRUD.readProduct(productId);
+        stockSystem.addToStock(prod, pointOfSaleId, units);
     }
 
     @Override
