925e0ccd9d48410aa0ac885291f48d7ae54dc338
Oemer Bilgit <s67173@bht-berlin.de>
2024-07-16 02:23:15 +0200

    pat 1 without requirement 7

--- org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
+++ org.dieschnittstelle.ess.mip.client/src/main/java/org/dieschnittstelle/ess/mip/client/shopping/PurchaseServiceClient.java
@@ -1,6 +1,11 @@
 package org.dieschnittstelle.ess.mip.client.shopping;
 
 import org.apache.logging.log4j.Logger;
+import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.dieschnittstelle.ess.entities.shopping.ShoppingCartItem;
+import org.dieschnittstelle.ess.mip.client.apiclients.ServiceProxyFactory;
+import org.dieschnittstelle.ess.mip.client.apiclients.ShoppingCartClient;
+import org.dieschnittstelle.ess.mip.components.shopping.api.PurchaseService;
 import org.dieschnittstelle.ess.mip.components.shopping.api.ShoppingException;
 import org.dieschnittstelle.ess.entities.crm.AbstractTouchpoint;
 import org.dieschnittstelle.ess.entities.crm.Customer;
@@ -18,30 +23,41 @@ public class PurchaseServiceClient implements ShoppingBusinessDelegate {
 	 *  in this case and access the shopping cart using the provided getter method
 	 */
 
+	private AbstractTouchpoint touchpoint;
+	private Customer customer;
+	private ShoppingCartClient shoppingCartClient;
+	private PurchaseService purchaseServiceProxy;
+
 	public PurchaseServiceClient() {
-		/* TODO: instantiate the proxy using the ServiceProxyFactory (see the other client classes) */
+		try {
+			/* TODO: instantiate the proxy using the ServiceProxyFactory (see the other client classes) */
+			purchaseServiceProxy = ServiceProxyFactory.getInstance().getProxy(PurchaseService.class);
+			this.shoppingCartClient = new ShoppingCartClient();
+		} catch (Exception e) {
+			throw new RuntimeException("Got exception: " + e, e);
+		}
 	}
 
 	/* TODO: implement the following methods s */
 
 	@Override
 	public void setTouchpoint(AbstractTouchpoint touchpoint) {
-	
+		this.touchpoint = touchpoint;
 	}
 
 	@Override
 	public void setCustomer(Customer customer) {
-	
+		this.customer = customer;
 	}
 
 	@Override
 	public void addProduct(AbstractProduct product, int units) {
-	
+		this.shoppingCartClient.addItem(new ShoppingCartItem(product.getId(), units, product instanceof Campaign));
 	}
 
 	@Override
 	public void purchase() throws ShoppingException {
-	
+		this.purchaseServiceProxy.purchaseCartAtTouchpointForCustomer(this.shoppingCartClient.getShoppingCartEntityId(), this.touchpoint.getId(), this.customer.getId());
 	}
 
 }
